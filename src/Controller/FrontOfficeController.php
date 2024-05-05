1096c8e4d7bfb33974b042b1f9fb3ece0596ac9c
Aya Boukhris <72460056+Aya-Bs@users.noreply.github.com>
2024-05-05 21:19:37 +0100

    +user commit

--- src/Controller/FrontOfficeController.php
+++ src/Controller/FrontOfficeController.php
@@ -3,9 +3,12 @@
 namespace App\Controller;
 
 use App\Repository\DebtRepository;
+use App\Repository\TaskRepository;
+use App\Repository\TodolistRepository;
 use Symfony\Bundle\FrameworkBundle\Controller\AbstractController;
 use Symfony\Component\HttpFoundation\Response;
 use Symfony\Component\Routing\Annotation\Route;
+
 
 class FrontOfficeController extends AbstractController
 {
@@ -16,7 +19,8 @@ class FrontOfficeController extends AbstractController
             'controller_name' => 'FrontOfficeController',
         ]);
     }
-    #[Route('/front/office/Debt', name: 'app_front_office_Debt')]
+
+    #[Route('/front/office/debt', name: 'app_front_office_Debt')]
     public function indexDebt(DebtRepository $debtRepository): Response
     {
         $debts = $debtRepository->findAll(); // Fetch all debts from the database
@@ -26,4 +30,32 @@ class FrontOfficeController extends AbstractController
             'controller_name' => 'FrontOfficeController',
         ]);
     }
+    #[Route('/front/office/Wishlist', name: 'app_front_office_Wishlist')]
+    public function indexWishlist(): Response
+    {
+         // Fetch all debts from the database
+
+        return $this->render('front_office/indexWishlist.html.twig', [
+           
+            'controller_name' => 'FrontOfficeController',
+        ]);
+    }
+    #[Route('/front/office/task', name: 'app_front_office_Task')]
+    public function indexTask(TaskRepository $taskRepository): Response
+    {
+        return $this->render('front_office/indexTask.html.twig', [
+            'tasks' => $taskRepository->findAll(),
+            'controller_name' => 'FrontOfficeController',
+        ]);
+    }
+
+    #[Route('/front/office/todo', name: 'app_front_office_Todo')]
+    public function indexTodo(TodolistRepository $todolistRepository): Response
+    {
+        return $this->render('front_office/indexTodo.html.twig', [
+            'todolists' => $todolistRepository->findAll(),
+            'controller_name' => 'FrontOfficeController',
+        ]);
+    }
+
 }
