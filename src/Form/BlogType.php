b71c82cd10ea422885cafe8429c7d0b1abeb9e0c
Aya Boukhris <72460056+Aya-Bs@users.noreply.github.com>
2024-05-05 21:21:21 +0100

    +user commit

--- src/Form/BlogType.php
+++ src/Form/BlogType.php
@@ -6,6 +6,9 @@ use App\Entity\Blog;
 use Symfony\Component\Form\AbstractType;
 use Symfony\Component\Form\FormBuilderInterface;
 use Symfony\Component\OptionsResolver\OptionsResolver;
+use Symfony\Component\Form\Extension\Core\Type\TextareaType;
+use Symfony\Component\Form\Extension\Core\Type\FileType;
+
 
 class BlogType extends AbstractType
 {
@@ -13,11 +16,16 @@ class BlogType extends AbstractType
     {
         $builder
             ->add('title')
-            ->add('content')
+            ->add('content', TextareaType::class, [
+                'attr' => ['placeholder' => 'Lorem ipsum dolor sit amet, consectetur adipiscing elit. Quisque quam neque, auctor vitae consequat a, condimentum in ante. Duis dictum iaculis malesuada. Vivamus ultrices erat nibh, id pretium ante mattis et. Donec malesuada elit at semper suscipit. Etiam sed lacus sit amet nisi aliquet tempus.'], // Add placeholder here
+            ])
             ->add('datePublished')
-            ->add('nbrOfLikes')
-            ->add('blogImage')
-            ->add('tags')
+            ->add('imageFile', FileType::class, [
+                'label' => 'Blog Image', // Add label for the file upload field
+                'required' => true, // Set to false if the image is optional
+                'mapped' => false, // Set to false to prevent mapping to entity property
+            ])
+            ->add('tags');
         ;
     }
 
